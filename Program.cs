19f85a9146fac15d990c582f1a881e470768d731
Dmitry_Malyasov <dmalyasov24@gmail.com>
2022-08-19 11:07:38 +0800

    Task done

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿void Pow3(int number)
+{
+    for (int i = 0; i <= number; ++i)
+    Console.WriteLine( i + "   " + Math.Pow(i, 3) );
+    Console.WriteLine();
+}
+
+int number = 3;
+Pow3(number);
+
+number = 5;
+Pow3(number);
